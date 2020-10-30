6011c988e8003d3cbebde751b5b2ee185c3494d3
Suryansh Purohit <suryansh.purohit3112@gmail.com>
2020-10-31 03:57:15 +0530

    browser restriction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import FullScreen from './components/FullScreen'
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>This is the main page</h1>
+      <input type="text " />
+      <div className="fss">
+      <FullScreen/>
+      </div>
+      <button onChange={()=>{
+        document.querySelector(".fss").requestFullscreen()
+      }}></button>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/FullScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FullScreen() {
+  return (
+    <div style={{backgroundColor:"red",height:"5rem",width:"5rem"}}>
+      skdsfklnsldnfjndkjbkb
+    </div>
+  )
+}
+
+export default FullScreen
