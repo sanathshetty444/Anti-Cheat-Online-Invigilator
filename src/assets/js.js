aa992eb553f2921ada3b9c554397f0e55222e0d5
Assasin10 <65104932+Assasin10@users.noreply.github.com>
2020-10-31 10:53:00 +0530

    updated

--- src/assets/js.js
+++ src/assets/js.js
@@ -1,17 +1,1 @@
-import Webcam from 'webcam-easy';
-
-
-
-const webcamElement = document.getElementById('webcam');
-const canvasElement = document.getElementById('canvas');
-const snapSoundElement = document.getElementById('snapSound');
-const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
-webcam.start()
-   .then(result =>{
-      console.log("webcam started");
-   })
-   .catch(err => {
-       console.log(err);
-   });
-var picture = webcam.snap();
-webcam.stop();
+// 
