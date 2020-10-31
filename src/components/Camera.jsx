e7b74b4cdccab1938ffb1eebec704b0faa0107b2
Ayush Devendra Tomar <41674634+ayushtom@users.noreply.github.com>
2020-11-01 04:53:21 +0530

    react integration

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,48 +1,116 @@
 import React from 'react';
+import {Component} from 'react'
 const faceapi =require('face-api.js')
-export default function Camera(){
-    const video = document.getElementById('videoInput')
-    // const faceapi =require('ngx-face-api-js')
+const canvas = require('canvas');
+// faceapi.env.monkeyPatch({ canvas, Image })
+// global.Blob = require('blob');
+// export default function Camera(){
+//     const video = document.getElementById('videoInput')
+//     // const faceapi =require('ngx-face-api-js')
+
+//     async function sample()
+//     {
+//         const a=Promise.all([
+//             faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
+//             faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
+//             faceapi.nets.ssdMobilenetv1.loadFromUri('../models') ,//heavier/accurate version of tiny face detector
+//             console.log("yay1"),
+//             start()
+//         ])
+
+//         function start() {
+//             console.log("yay")
     
-    Promise.all([
-        faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
-        faceapi.nets.ssdMobilenetv1.loadFromUri('../models') //heavier/accurate version of tiny face detector
-    ]).then(start)
     
-    function start() {
-        document.body.append('Models Loaded')
+//             document.body.append('Models Loaded')
+            
+//             navigator.getUserMedia(
+//                 { video:{} },
+//                 stream => video.srcObject = stream,
+//                 err => console.error(err)
+//             )
+            
+//             // video.src = '../videos/speech.mp4'
+//             console.log('video added')
+//             recognizeFaces()
+//         }
         
-        navigator.getUserMedia(
-            { video:{} },
-            stream => video.srcObject = stream,
-            err => console.error(err)
-        )
-        
-        // video.src = '../videos/speech.mp4'
-        console.log('video added')
-        recognizeFaces()
+
+//     }
+   
+    
+    
+    
+    
+   
+
+//     sample()
+//     return(
+//     <video id="videoInput" width="720" height="550" muted controls></video>
+//     )
+// } 
+
+
+
+export default class Camera extends Component {
+    constructor(props) {
+      super(props);
+      this.videoTag = React.createRef()
+
+
+      var promise = new Promise(function(resolve, reject) {
+        // call resolve if the method succeeds
+        faceapi.nets.faceRecognitionNet.loadFromUri('../models')
+        faceapi.nets.faceLandmark68Net.loadFromUri('../models')
+        faceapi.nets.ssdMobilenetv1.loadFromUri('../models')
+        resolve();
+      })
+      promise.then(this.recognizeFaces(this))
     }
-    
-    async function recognizeFaces() {
-    
-        const labeledDescriptors = await loadLabeledImages()
+
+    async loadLabeledImages() {
+
+        const labels = ['Black Widow', 'Ganesh Reddy','Ayush Tomar','Prashant Kumar']
+        // const labels = ['Prashant Kumar'] // for WebCam
+        return Promise.all(
+            labels.map(async (label)=>{
+                const descriptions = []
+                for(let i=1; i<=2; i++) {
+
+                    // const img = await faceapi.fetchImage('http://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-Javascript/master/labeled_images/')
+                    const img = await canvas.loadImage(`./1.jpg`);
+                    
+                    console.log(img+"image aagaya")
+
+                    const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                    console.log(label + i + JSON.stringify(detections))
+                    descriptions.push(detections.descriptor)
+                }
+                document.body.append(label+' Faces Loaded | ')
+                return new faceapi.LabeledFaceDescriptors(label, descriptions)
+            })
+        )
+    }
+
+    async recognizeFaces() {
+
+        const labeledDescriptors = await this.loadLabeledImages(this)
         console.log(labeledDescriptors)
         const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.7)
     
     
-        video.addEventListener('play', async () => {
+        this.video.addEventListener('play', async () => {
             console.log('Playing')
-            const canvas = faceapi.createCanvasFromMedia(video)
+            const canvas = faceapi.createCanvasFromMedia(this.video)
             document.body.append(canvas)
     
-            const displaySize = { width: video.width, height: video.height }
+            const displaySize = { width: this.video.width, height: this.video.height }
             faceapi.matchDimensions(canvas, displaySize)
     
             
     
             setInterval(async () => {
-                const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+                const detections = await faceapi.detectAllFaces(this.video).withFaceLandmarks().withFaceDescriptors()
     
                 const resizedDetections = faceapi.resizeResults(detections, displaySize)
     
@@ -54,6 +122,7 @@ export default function Camera(){
                 results.forEach( (result, i) => {
                     const box = resizedDetections[i].detection.box
                     const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+                    console.log(result.toString())
                     drawBox.draw(canvas)
                 })
             }, 100)
@@ -62,32 +131,28 @@ export default function Camera(){
             
         })
     }
+
+
+     
     
-    
-    function loadLabeledImages() {
-        const labels = ['Black Widow', 'Ganesh Reddy','Ayush Tomar','Prashant Kumar']
-        // const labels = ['Prashant Kumar'] // for WebCam
-        return Promise.all(
-            labels.map(async (label)=>{
-                const descriptions = []
-                for(let i=1; i<=2; i++) {
-                    const img = await faceapi.fetchImage(`../labeled_images/${label}/${i}.jpg`)
-                    const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                    console.log(label + i + JSON.stringify(detections))
-                    descriptions.push(detections.descriptor)
-                }
-                document.body.append(label+' Faces Loaded | ')
-                return new faceapi.LabeledFaceDescriptors(label, descriptions)
-            })
-        )
+  
+    componentDidMount() {
+      // getting access to webcam
+     navigator.mediaDevices
+      .getUserMedia({video: true})
+      .then(stream => this.videoTag.current.srcObject = stream)
+      .catch(console.log);
     }
 
 
-
-
-
-
-    return(
-    <video id="videoInput" width="720" height="550" muted controls></video>
-    )
-} 
+     
+  
+    render() {
+      return (
+        <video 
+          ref={this.videoTag}
+          autoPlay
+        />
+      )
+    }
+  }
